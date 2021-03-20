81ab4fcf0446e2c26b001e0ff9f133b0250d24e5
BigRob <robertcmoore92@gmail.com>
2021-03-20 12:31:58 -0400

    added books

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ const authors = [
 	{ id: 2, name: 'J. R. R. Tolkien' },
     { id: 3, name: 'Brent Weeks' }
 ]
+
+const books = [
+	{ id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+	{ id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
