3627c7ed68c54203cf0b6505421d8b5e788e6d3d
BigRob <robertcmoore92@gmail.com>
2021-03-20 12:32:42 -0400

    added books

--- server.js
+++ server.js
@@ -25,3 +25,6 @@ const books = [
 	{ id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
     { id: 5, name: 'The Two Towers', authorId: 2 },
 	{ id: 6, name: 'The Return of the King', authorId: 2 },
+    { id: 7, name: 'The Way of Shadows', authorId: 3 },
+	{ id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
