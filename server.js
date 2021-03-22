82f1edd62a45178e1d700d0c311bd65948f7445e
BigRob <robertcmoore92@gmail.com>
2021-03-22 10:41:35 -0400

    added id and name

--- server.js
+++ server.js
@@ -33,3 +33,5 @@ const BookType = new GraphQLObjectType({
     name: 'Book',
     description: 'This represents a book written by an author',
     fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLInt) },
+        name: { type: GraphQLNonNull(GraphQLString) },
